8996401d1ec68aa373e620d914d0255a81ac9663
Никита Коняев <cinema4dmipad@gmail.com>
2026-06-24 01:08:19 +0300

    compose changes

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -76,6 +76,11 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
     ),
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework.parsers.JSONParser',
+        'rest_framework.parsers.FormParser',
+        'rest_framework.parsers.MultiPartParser',
+    ),
 }
 
 DJOSER = {
